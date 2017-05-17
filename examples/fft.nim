e4886dfb71a8d9ffe2e052534005c947e6631646
Andrea Ferretti <ferrettiandrea@gmail.com>
2017-05-17 16:47:21 +0200

    Wokring FFT example

--- examples/fft.nim
+++ examples/fft.nim
@@ -20,34 +20,55 @@ template check(a: cufftResult) =
   let y = a
   if y != CUFFT_SUCCESS: raiseCufftError(y)
 
+# {. passl: "-lcublas" passl: "-lcudart" .}
+#
+# proc cudaMalloc(p: ptr pointer, size: int): cudaError
+#   {. header: "cuda_runtime_api.h", importc: "cudaMalloc" .}
+#
+# proc cudaFree(p: pointer): cudaError
+#   {. header: "cuda_runtime_api.h", importc: "cudaFree" .}
+#
+# proc cudaMalloc32(size: int): ptr float32 =
+#   let s = size * sizeof(float32)
+#   check cudaMalloc(cast[ptr pointer](addr result), s)
+
 proc main() =
   const
     NX = 256
     NY = 128
     N = NX * NY
+  let size = sizeof(cufftComplex) * N
   var
     plan: cufftHandle
     idata: ptr cufftComplex
     odata: ptr cufftComplex
 
-  check cudaMalloc(cast[ptr pointer](addr idata), sizeof(cufftComplex) * N)
-  check cudaMalloc(cast[ptr pointer](addr odata), sizeof(cufftComplex) * N)
+  check cudaMalloc(cast[ptr pointer](addr idata), size)
+  check cudaMalloc(cast[ptr pointer](addr odata), size)
 
-  var idatap = cast[CPointer[cufftComplex]](idata)
+  var
+    input = newSeq[cufftComplex](N)
+    output = newSeq[cufftComplex](N)
 
-  for i in 0 ..< N:
-    idatap[i].x = cfloat(i) / cfloat(N)
-    idatap[i].y = cfloat(N - i) / cfloat(N)
+  for i in 0 ..< input.len:
+    input[i].x = cfloat(i) / cfloat(N)
+    input[i].y = cfloat(N - i) / cfloat(N)
+
+  check cudaMemcpy(idata, addr input[0], size, cudaMemcpyHostToDevice)
 
   check cufftPlan2d(addr plan, NX, NY, CUFFT_C2C)
   check cufftExecC2C(plan, idata, odata, CUFFT_FORWARD)
   check cufftExecC2C(plan, odata, odata, CUFFT_INVERSE)
 
-  echo "FFT: "
+  check cudaMemcpy(addr output[0], odata, size, cudaMemcpyDeviceToHost)
 
   check cufftDestroy(plan)
   check cudaFree(idata)
   check cudaFree(odata)
+
+  echo "original : ", input[0..10]
+  echo "transform: ", output[0..10]
+
 
 
 when isMainModule:
