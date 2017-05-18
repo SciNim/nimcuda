8340bb90497ece1f4b621296105010a8cbd69301
Andrea Ferretti <ferrettiandrea@gmail.com>
2017-05-18 16:14:51 +0200

    Added exceptions

--- nimcuda/nimcuda.nim
+++ nimcuda/nimcuda.nim
@@ -2,6 +2,7 @@ import ./cublas_api
 import ./cublas_v2
 import ./cuComplex
 import ./cuda_runtime_api
+import ./cudnn
 import ./cufft
 import ./cusolver_common
 import ./cusolverDn
@@ -19,12 +20,19 @@ import ./vector_types
 type
   CudaError* = object of IOError
   CufftError* = object of IOError
+  CublasError* = object of IOError
   CusparseError* = object of IOError
+  CusolverError* = object of IOError
+  CudnnError* = object of IOError
   NVGraphError* = object of IOError
 
 template check*(a: cudaError_t) =
   if a != cudaSuccess:
     raise newException(CudaError, $a & " " & $int(a))
+
+template check*(a: cublasStatus_t) =
+  if a != CUBLAS_STATUS_SUCCESS:
+    raise newException(CublasError, $a & " " & $int(a))
 
 template check*(a: cufftResult) =
   if a != CUFFT_SUCCESS:
@@ -33,6 +41,14 @@ template check*(a: cufftResult) =
 template check*(a: cusparseStatus_t) =
   if a != CUSPARSE_STATUS_SUCCESS:
     raise newException(CusparseError, $a & " " & $int(a))
+
+template check*(a: cusolverStatus_t) =
+  if a != CUSOLVER_STATUS_SUCCESS:
+    raise newException(CusolverError, $a & " " & $int(a))
+
+template check*(a: cudnnStatus_t) =
+  if a != CUDNN_STATUS_SUCCESS:
+    raise newException(CudnnError, $a & " " & $int(a))
 
 template check*(a: nvgraphStatus_t) =
   if a != NVGRAPH_STATUS_SUCCESS:
